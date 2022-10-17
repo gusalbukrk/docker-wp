34d682844ebbfb5b1cdc1add2e52e84ad577d71e
gusalburk <gusalbukrk@gmail.com>
2022-10-17 08:31:26 -0300

    format `Dockerfile/script.sh`

--- Dockerfile/script.sh
+++ Dockerfile/script.sh
@@ -6,8 +6,32 @@ if [[ $# != 2 ]]; then
 fi
 
 docker network create wp-network;
-docker build --tag db -f Dockerfile.db .;
-docker build --tag wp -f Dockerfile.wp .;
-docker run -d --name db --env MYSQL_ROOT_PASSWORD=$1 --env MYSQL_PASSWORD=$2 --network wp-network --volume db-data:/var/lib/mysql -p 3306:3306 db;
-docker run -d --name admin --network wp-network --link db:db -p 82:80 phpmyadmin:5.2.0;
-docker run -d --name wp --env WORDPRESS_DB_PASSWORD=$2 --network wp-network --volume wp-data:/var/www/html -p 80:80 wp;
+
+docker build --tag db \
+  -f Dockerfile.db \
+  .;
+
+docker build --tag wp \
+  -f Dockerfile.wp \
+  .;
+
+docker run -d --name db \
+  --env MYSQL_ROOT_PASSWORD=$1 \
+  --env MYSQL_PASSWORD=$2 \
+  --network wp-network \
+  --volume db-data:/var/lib/mysql \
+  -p 3306:3306 \
+  db
+
+docker run -d --name wp \
+  --env WORDPRESS_DB_PASSWORD=$2 \
+  --network wp-network \
+  --volume wp-data:/var/www/html \
+  -p 80:80 \
+  wp;
+
+docker run -d --name admin \
+  --network wp-network \
+  --link db:db \
+  -p 82:80 \
+  phpmyadmin:5.2.0;
